e0dbe6cab84860b7e859d4680f80efae44688c80
TomoGaSukunai <292768995@qq.com>
2017-03-18 17:10:10 +0800

    Add try catch

--- pic2html.js
+++ pic2html.js
@@ -1,16 +1,21 @@
 var fs = require("fs")
 var path = require("path")
+try{
 var pics = fs.readdirSync(path.join(__dirname, "web", "pic"))
 console.log(pics)
 var str =""
-str += "<meta charset='utf=8'>"
-str += "<html>"
+str += "<meta charset='utf=8'>\n"
+str += "<html>\n"
+str += "<style>a{font: 40px;}</style>"
 
 for (var pic of pics){
-    str+="<a href='./pic/" + pic + "'>"+pic.split(".")[0]+"</a>"
+    str+="<a href='./pic/" + pic + "'>"+pic.split(".")[0]+"</a>\n"
 }
 
-str += "</html>"
+str += "</html>\n"
 
 console.log(str)
 fs.writeFile(path.join(__dirname,"web","pic.html") ,str)
+}catch(err){
+    console.log(err)
+}
