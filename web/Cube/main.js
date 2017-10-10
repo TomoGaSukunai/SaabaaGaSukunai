85aa94b649d708439a0308e392aa31e7ee222adc
TomoGaSukunai <292768995@qq.com>
2017-10-10 17:31:39 +0900

    Work around with Edge and formatted

--- web/Cube/main.js
+++ web/Cube/main.js
@@ -2,92 +2,92 @@ var myCanvas = document.getElementById("myCanvas")
 myCanvas.width = myCanvas.clientWidth
 myCanvas.height = myCanvas.clientHeight
 
-function getEye(){
+function getEye() {
     return [
-        1,0,0,0,
-        0,1,0,0,
-        0,0,1,0,
-        0,0,0,1,
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
     ]
 }
-function get_projection(angle, a, zMin, zMax){
-    var ang = Math.tan((angle * 0.5)*Math.PI/180)
+function get_projection(angle, a, zMin, zMax) {
+    var ang = Math.tan((angle * 0.5) * Math.PI / 180)
     return [
-        0.5/ang,0,0,0,
-        0,0.5*a/ang,0,0,
-        0,0,-(zMax+zMin)/(zMax-zMin), -1,
-        0,0,(-2*zMax*zMin)/(zMax-zMin),0,
+        0.5 / ang, 0, 0, 0,
+        0, 0.5 * a / ang, 0, 0,
+        0, 0, -(zMax + zMin) / (zMax - zMin), -1,
+        0, 0, (-2 * zMax * zMin) / (zMax - zMin), 0,
     ]
 }
 
-addEventListener("resize", function(){
+addEventListener("resize", function () {
     myCanvas.width = myCanvas.clientWidth
-    myCanvas.height =  myCanvas.clientHeight
-    project_matrix = get_projection(40,myCanvas.width/myCanvas.height,1,100)
+    myCanvas.height = myCanvas.clientHeight
+    project_matrix = get_projection(40, myCanvas.width / myCanvas.height, 1, 100)
 })
 
-function initGL(canvas){
-    gl = canvas .getContext("webgl")
-    if (gl === null){
+function initGL(canvas) {
+    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
+    if (gl === null) {
         alert("Your broswer does not support webgl")
-        throw new Error("no gl error")   
+        throw new Error("no gl error")
     }
     return gl
 }
 
-function createBlocks(CubeData){
+function createBlocks(CubeData) {
     var Blocks = []
-    for (var i in CubeData.vertices_f){
+    for (var i in CubeData.vertices_f) {
         var vertices = []
         var colors = []
         var indices = []
         var v0 = CubeData.vertices[i]
         //create 3 outer faces
-        for (var j=0; j<3; j++){
+        for (var j = 0; j < 3; j++) {
             //
-            indices.push(CubeData.quad_indices.map(k=>k+vertices.length))
+            indices.push(CubeData.quad_indices.map(k => k + vertices.length))
             //
             var face = CubeData.faces[CubeData.vertices_f[i][j]]
-            var face_prev = CubeData.faces[CubeData.vertices_f[i][(j+2)%3]]
-            var face_next = CubeData.faces[CubeData.vertices_f[i][(j+1)%3]]
+            var face_prev = CubeData.faces[CubeData.vertices_f[i][(j + 2) % 3]]
+            var face_next = CubeData.faces[CubeData.vertices_f[i][(j + 1) % 3]]
             vertices.push(v0)
-            vertices.push(face.map((x,k)=>x+face_prev[k]))
+            vertices.push(face.map((x, k) => x + face_prev[k]))
             vertices.push(face)
-            vertices.push(face.map((x,k)=>x+face_next[k]))            
+            vertices.push(face.map((x, k) => x + face_next[k]))
             //with color of face
             var c = CubeData.faces_color[CubeData.vertices_f[i][j]]
-            colors.push(c,c,c,c)
+            colors.push(c, c, c, c)
         }
         // create inner faces
         //
-        indices.push(CubeData.conner_indices.map(k=>k+vertices.length))
+        indices.push(CubeData.conner_indices.map(k => k + vertices.length))
         //
-        vertices.push([0,0,0])
-        for (var j=0; j<3;j++){
-            vertices.push(vertices[4*j+1])
-            vertices.push(vertices[4*j+2])        
+        vertices.push([0, 0, 0])
+        for (var j = 0; j < 3; j++) {
+            vertices.push(vertices[4 * j + 1])
+            vertices.push(vertices[4 * j + 2])
         }
         //
-        var c = [0.9,0.9,0.9]
-        colors.push(c,c,c,c,c,c,c)
-        
+        var c = [0.9, 0.9, 0.9]
+        colors.push(c, c, c, c, c, c, c)
+
         //move block a bit away from origin point
-        vertices = vertices.map(v=>v.map((k,i)=>k + v0[i]*0.1))
+        vertices = vertices.map(v => v.map((k, i) => k + v0[i] * 0.1))
 
         //
-        vertices = vertices.reduce((a,b)=>[...a,...b])
-        indices = indices.reduce((a,b)=>[...a,...b])
-        colors = colors.reduce((a,b)=>[...a,...b])
+        vertices = vertices.reduce((a, b) => [...a, ...b])
+        indices = indices.reduce((a, b) => [...a, ...b])
+        colors = colors.reduce((a, b) => [...a, ...b])
 
-        var Block = {vertices, indices, colors}
+        var Block = { vertices, indices, colors }
         Block.idx = parseInt(i)
-        
+
         Blocks.push(Block)
     }
     return Blocks
 }
 
-function bufferInit(Obj, gl = gl){
+function bufferInit(Obj, gl = gl) {
     Obj.move_matrix = getEye()
 
     var vertex_buffer = gl.createBuffer()
@@ -110,7 +110,7 @@ function bufferInit(Obj, gl = gl){
     Obj.index_buffer = index_buffer
 }
 
-function shaderInit(vertSource, fragSource, gl = gl){
+function shaderInit(vertSource, fragSource, gl = gl) {
     var vertShader = gl.createShader(gl.VERTEX_SHADER)
     gl.shaderSource(vertShader, vertSource)
     gl.compileShader(vertShader)
@@ -128,41 +128,41 @@ function shaderInit(vertSource, fragSource, gl = gl){
     return shaderProgram
 }
 
-function rotateZ(m, angle){
+function rotateZ(m, angle) {
     var c = Math.cos(angle)
     var s = Math.sin(angle)
     var mv0 = m[0], mv4 = m[4], mv8 = m[8];
-    m[0] = c*m[0] - s*m[1]
-    m[4] = c*m[4] - s*m[5]
-    m[8] = c*m[8] - s*m[9]
-    m[1] = c*m[1] + s*mv0
-    m[5] = c*m[5] + s*mv4
-    m[9] = c*m[9] + s*mv8   
+    m[0] = c * m[0] - s * m[1]
+    m[4] = c * m[4] - s * m[5]
+    m[8] = c * m[8] - s * m[9]
+    m[1] = c * m[1] + s * mv0
+    m[5] = c * m[5] + s * mv4
+    m[9] = c * m[9] + s * mv8
 }
-function rotateX(m, angle){
+function rotateX(m, angle) {
     var c = Math.cos(angle)
     var s = Math.sin(angle)
     var mv1 = m[1], mv5 = m[5], mv9 = m[9];
-    m[1] = c*m[1] - s*m[2]
-    m[5] = c*m[5] - s*m[6]
-    m[9] = c*m[9] - s*m[10]
-    m[2] = c*m[2] + s*mv1
-    m[6] = c*m[6] + s*mv5
-    m[10] = c*m[10] + s*mv9   
+    m[1] = c * m[1] - s * m[2]
+    m[5] = c * m[5] - s * m[6]
+    m[9] = c * m[9] - s * m[10]
+    m[2] = c * m[2] + s * mv1
+    m[6] = c * m[6] + s * mv5
+    m[10] = c * m[10] + s * mv9
 }
-function rotateY(m, angle){
+function rotateY(m, angle) {
     var c = Math.cos(angle)
     var s = Math.sin(angle)
     var mv2 = m[2], mv6 = m[6], mv10 = m[10];
-    m[2] = c*m[2] - s*m[0]
-    m[6] = c*m[6] - s*m[4]
-    m[10] = c*m[10] - s*m[8]
-    m[0] = c*m[0] + s*mv2
-    m[4] = c*m[4] + s*mv6
-    m[8] = c*m[8] + s*mv10   
+    m[2] = c * m[2] - s * m[0]
+    m[6] = c * m[6] - s * m[4]
+    m[10] = c * m[10] - s * m[8]
+    m[0] = c * m[0] + s * mv2
+    m[4] = c * m[4] + s * mv6
+    m[8] = c * m[8] + s * mv10
 }
 
-function draw(){
+function draw() {
     gl.viewport(0, 0, myCanvas.width, myCanvas.height)
     gl.enable(gl.DEPTH_TEST)
     gl.clearColor(0, 0, 0, 1)
@@ -173,84 +173,89 @@ function draw(){
     timeStamp = now
     gl.uniformMatrix4fv(Pmatrix, false, project_matrix)
     gl.uniformMatrix4fv(Vmatrix, false, view_matrix)
-    
+
     //rotateY(move_matrix, 0.01*60/1000*dt)
     rotateY(move_matrix, rY)
     rotateX(move_matrix, rX)
 
     gl.uniformMatrix4fv(Mmatrix, false, move_matrix)
-    
+
     move_matrix = getEye()
 
-    if (!keyState.lock && keyState.f){
+    if (!keyState.lock && keyState.f) {
         var bool = true
         var axis = 0
         var rot = false
         if (animation_que.length == 0)
             rot = true
-        switch(keyState.code){
-            case "KeyQ": bool = false
-            case "KeyA": axis = 0
+        switch (keyState.code) {
+            case "KeyQ": case "Q": bool = false
+            case "KeyA": case "A": axis = 0
                 break
-            case "KeyW": bool = false
-            case "KeyS": axis = 1
+            case "KeyW": case "W": bool = false
+            case "KeyS": case "S": axis = 1
                 break
-            case "KeyE": bool = false
-            case "KeyD": axis = 2
+            case "KeyE": case "E": bool = false
+            case "KeyD": case "D": axis = 2
                 break
-            case "KeyR": bool = false
-            case "KeyF": axis = 3
+            case "KeyR": case "R": bool = false
+            case "KeyF": case "F": axis = 3
                 break
-            case "KeyT": bool = false
-            case "KeyG": axis = 4
+            case "KeyT": case "T": bool = false
+            case "KeyG": case "G": axis = 4
                 break
-            case "KeyY": bool = false
-            case "KeyH": axis = 5
+            case "KeyY": case "Y": bool = false
+            case "KeyH": case "H": axis = 5
                 break
-            case "ArrowLeft": 
+            case "ArrowLeft":
+            case "LEFT":
                 rY -= 0.1
                 rot = false
                 break
             case "ArrowRight":
+            case "RIGHT":
                 rY += 0.1
                 rot = false
                 break
-            case "ArrowUp": 
-                rX -= 0.1
+            case "ArrowUp":
+            case "UP":
+                rX = Math.max(-Math.PI / 3, rX - 0.1)
                 rot = false
                 break
             case "ArrowDown":
-                rX += 0.1
+            case "DOWN":
+                rX = Math.min(Math.PI / 3, rX + 0.1)
                 rot = false
                 break
             case "Space":
+            case " ":
                 //TODO
                 keyState.lock = true
                 xhrTest()
                 rot = false
                 break
-            default:{
+            default: {
                 rot = false
-                console.log(keyState.code)                
+                console.log(keyState.code)
             }
-        }    
-        if(rot){
+        }
+        if (rot) {
             rotateCubeAnimation(axis, bool)
-        }else{
+        } else {
             //console.log(rX,rY)
         }
         keyState.f = false
     }
 
-    if (animation_que.length > 0){        
-        if (animation_que[0](dt)){
-            animation_que.splice(0,1)
+    if (animation_que.length > 0) {
+        if (animation_que[0](dt)) {
+            animation_que.splice(0, 1)
         }
-    }else{
+    } else {
         keyState.lock = false
     }
 
-    for(var idx in Blocks){
+    for (var idx in Blocks) {
         //if (idx == 0 ) continue
         var Block = Blocks[idx]
 
@@ -261,9 +266,9 @@ function draw(){
         gl.bindBuffer(gl.ARRAY_BUFFER, Block.vertex_buffer)
         gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0)
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, Block.color_buffer)       
+        gl.bindBuffer(gl.ARRAY_BUFFER, Block.color_buffer)
         gl.vertexAttribPointer(color, 3, gl.FLOAT, false, 0, 0)
-        
+
         gl.drawElements(gl.TRIANGLES, Block.indices.length, gl.UNSIGNED_SHORT, 0)
     }
 
@@ -271,31 +276,31 @@ function draw(){
 }
 
 
-function rotateCubeAnimation(i, b = true){
-    var f =  function(dt){        
+function rotateCubeAnimation(i, b = true) {
+    var f = function (dt) {
         var face = CubeData.faces[i]
         var t0 = 120
-        var r = (b ? -1:1)/t0
+        var r = (b ? -1 : 1) / t0
         var t = t0
-        return function(dt){
+        return function (dt) {
             var dt = Math.min(t, dt)
             t -= dt
-            for(var maplet of CubeData.mapping[i]){
-                var src = b ? maplet[0] : maplet[1]                
-                var Block = Blocks[src]                
-                rotateX(Block.move_matrix, Math.PI/2 * face[0]*r*dt)
-                rotateY(Block.move_matrix, Math.PI/2 * face[1]*r*dt)
-                rotateZ(Block.move_matrix, Math.PI/2 * face[2]*r*dt)
+            for (var maplet of CubeData.mapping[i]) {
+                var src = b ? maplet[0] : maplet[1]
+                var Block = Blocks[src]
+                rotateX(Block.move_matrix, Math.PI / 2 * face[0] * r * dt)
+                rotateY(Block.move_matrix, Math.PI / 2 * face[1] * r * dt)
+                rotateZ(Block.move_matrix, Math.PI / 2 * face[2] * r * dt)
             }
-            if (t == 0){
-                var tBlocks = Blocks.map(x=>x);
-                for(var maplet of CubeData.mapping[i]){
+            if (t == 0) {
+                var tBlocks = Blocks.map(x => x);
+                for (var maplet of CubeData.mapping[i]) {
                     var src = b ? maplet[0] : maplet[1]
                     var dst = b ? maplet[1] : maplet[0]
                     tBlocks[dst] = Blocks[src]
                 }
                 Blocks = tBlocks
-                cubeStatus = mappingStatus(i,b,cubeStatus)
+                cubeStatus = mappingStatus(i, b, cubeStatus)
                 //blocksCheck()
                 return true
             }
@@ -305,21 +310,21 @@ function rotateCubeAnimation(i, b = true){
     animation_que.push(f)
 }
 
-function mappingStatus(i, b, status){
-    var nextStatus = status.map(x=>x)
-    for (var maplet of CubeData.mapping[i]){
+function mappingStatus(i, b, status) {
+    var nextStatus = status.map(x => x)
+    for (var maplet of CubeData.mapping[i]) {
         var src = b ? maplet[0] : maplet[1]
         var dst = b ? maplet[1] : maplet[0]
         var inc = b ? maplet[2] : (3 - maplet[2]) % 3
         nextStatus[dst] = status[src]
-        nextStatus[dst + 8] = (status[src + 8] + inc)%3
+        nextStatus[dst + 8] = (status[src + 8] + inc) % 3
     }
     return nextStatus;
 }
 
 var Blocks
 
-var project_matrix = get_projection(40,myCanvas.width/myCanvas.height,1,100)
+var project_matrix = get_projection(40, myCanvas.width / myCanvas.height, 1, 100)
 var view_matrix = getEye()
 view_matrix[14] = view_matrix[14] - 6
 var move_matrix = getEye()
@@ -337,13 +342,13 @@ var rY = -0.5
 var rX = 0.5
 var animation_que = []
 var cubeStatus = CubeData.getBytes(0)
-var keyState = {f: false, lock: false}
+var keyState = { f: false, lock: false }
 
-function main(){
+function main() {
     var gl = initGL(myCanvas)
 
     Blocks = createBlocks(CubeData)
-    for(var Block of Blocks){
+    for (var Block of Blocks) {
         bufferInit(Block, gl)
     }
 
@@ -362,9 +367,9 @@ function main(){
     Mmatrix = gl.getUniformLocation(shaderProgram, "Mmatrix")
     Lmatrix = gl.getUniformLocation(shaderProgram, "Lmatrix")
 
-    window.addEventListener("keydown",function(e){
+    window.addEventListener("keydown", function (e) {
         keyState.f = true
-        keyState.code = e.code
+        keyState.code = e.code || e.key.toUpperCase()
         //console.log(e)
     })
 
@@ -376,22 +381,22 @@ main()
 
 var info = document.getElementById("info")
 
-function xhrTest(){
+function xhrTest() {
     var xhr = new XMLHttpRequest()
     xhr.timeout = 5000
     xhr.open("POST", "/api/cube", true)
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-    xhr.send("code="+CubeData.getCode(cubeStatus))
+    xhr.send("code=" + CubeData.getCode(cubeStatus))
     info.style.display = "block"
     info.innerHTML = "Reciving slove..."
-    xhr.onreadystatechange = function(){
-        if (xhr.readyState == 4 && xhr.status == 200){
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
             info.style.display = "none"
-            JSON.parse(xhr.response).map(x=>rotateCubeAnimation(x%6, x<6))
-        }else if (xhr.readyState == 4 && xhr.status != 200){
+            JSON.parse(xhr.response).map(x => rotateCubeAnimation(x % 6, x < 6))
+        } else if (xhr.readyState == 4 && xhr.status != 200) {
             info.innerHTML = "failed to get solve from sevice, please retry"
-            setTimeout(function() {
-                info.style.display ="none"
+            setTimeout(function () {
+                info.style.display = "none"
             }, 2000);
         }
     }
